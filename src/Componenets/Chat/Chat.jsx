e579b391ef549082a82be8b311f3323c76b1d623
Sumit-Khamaru <sumitkhamaru7@gmail.com>
2022-12-16 18:35:35 +0530

    React Chat App frontend v1.0

---
+++ src/Componenets/Chat/Chat.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import "./Chat.css";
+import sendlogo from "../../Images/sendlogo.png";
+import { user } from "../Join/Join";
+import socketIo from "socket.io-client";
+import { useEffect } from "react";
+import { useState } from "react";
+import ReactScrollToBottom from "react-scroll-to-bottom";
+import Message from "../Message/Message";
+
+let socket;
+const ENDPOINT = "https://cchat-backend.onrender.com";
+
+const Chat = () => {
+  // const[chat, setChat] = useState("");
+  const [id, setId] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const send = () => {
+    const message = document.getElementById("chatInput").value;
+    socket.emit("message", { message, id });
+    document.getElementById("chatInput").value = "";
+  };
+
+  console.log(messages);
+
+  // Using the Socket to send data to backend
+  useEffect(() => {
+    socket = socketIo(ENDPOINT, { transports: [`websocket`] });
+
+    socket.on("connect", () => {
+      alert("Connected");
+      setId(socket.id);
+    });
+
+    socket.emit("joined", { user });
+
+    socket.on("welcome", (data) => {
+      setMessages([...messages, data]);
+      console.log(data.user, data.message);
+    });
+
+    socket.on("userJoined", (data) => {
+      setMessages([...messages, data]);
+      console.log(data.user, data.message);
+    });
+
+    socket.on("left", (data) => {
+      setMessages([...messages, data]);
+      console.log(data.user, data.message);
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.on("sendMessage", (data) => {
+      setMessages([...messages, data]);
+      console.log(data.user, data.message);
+    });
+
+    return () => {
+      socket.off();
+    };
+  }, [messages]);
+
+  return (
+    <div className="chatPage">
+      <div className="chatContainer">
+        <div className="header">
+          <h3>Chat App</h3>
+          <a href="/"><span className="material-symbols-rounded">close</span></a>
+        </div>
+        <ReactScrollToBottom className="chatBox">
+          {messages.map((item, i) => (
+            <Message
+              user={item.id === id ? "" : item.user}
+              message={item.message}
+              classs={item.id === id ? "right" : "left"}
+            />
+          ))}
+        </ReactScrollToBottom>
+        <div className="inputBox">
+          <input onKeyPress={(e) => e.key === 'Enter' ? send() : null} type="text" id="chatInput" />
+          <button className="sendBtn" onClick={send}>
+            <img src={sendlogo} alt="send" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
---
+++ src/Componenets/Join/Join.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Join.css";
+import logo from "../../Images/logo1.jpg";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+let user;
+const Join = () => {
+  
+  const [userName, setUserName] = useState('');
+
+  const joinHandler = () => {
+    user = userName;
+    setUserName("");
+    // console.log(userName);
+  }
+  return (
+    <div className="joinPage">
+      <div className="joinContainer">
+        <img src={logo} alt="logo" />
+        <h1>Chat App</h1>
+        <input type="text" id="joinInput" placeholder="Enter Your Name" value={userName} onChange={(e)=> setUserName(e.target.value)} />
+        <Link onClick={(e) => !userName ? e.preventDefault(): null} to="/chat"><button className="joinBtn" onClick={joinHandler}>Log In</button></Link>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
+export {user};
