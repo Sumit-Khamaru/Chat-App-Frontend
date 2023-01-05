16ad3aafd60133fae800c8be6a54ef2fd77946d5
Sumit Khamaru <sumitkhamaru7@gmail.com>
2023-01-05 22:37:54 +0530

    Chat App version 2.0

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = 'http://localhost:4000';
+export const host = 'https://cchat-backend.onrender.com';
 export const registerRoute = `${host}/api/auth/register`;
 
 export const loginRoute = `${host}/api/auth/login`;
